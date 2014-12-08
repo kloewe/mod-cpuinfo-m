c046550e9d460a1eee8813c83e5abd02131ae55b
Kristian Loewe <kl@kristianloewe.com>
2014-12-08 14:53:29 +0100

    Add test

---
+++ test/test.m
@@ -0,0 +1,24 @@
+addpath('../cpuinfo-m');
+
+which nproc
+which cpuinfo
+
+nproc
+
+cpuinfo('mmx')
+
+cpuinfo('sse')
+
+cpuinfo('sse2')
+
+cpuinfo('sse3')
+
+cpuinfo('ssse3')
+
+cpuinfo('sse41')
+
+cpuinfo('sse42')
+
+cpuinfo('popcnt')
+
+cpuinfo('avx')
